96a185509be5f9011c8d598dc9d19476bf91fbe5
Fabian Sisa <fabiansisa@hotmail.com>
2023-11-12 11:26:57 -0500

    Se agrega bootstrap y 5 componentes

--- angular-app-grupal/src/app/app.module.ts
+++ angular-app-grupal/src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactosComponent } from './contactos/contactos.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    BlogComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule
