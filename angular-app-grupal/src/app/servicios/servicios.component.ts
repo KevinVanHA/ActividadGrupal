081b49fc9a5d8f0fc79553eb575af30494919d43
KevinVanHA <118561655+KevinVanHA@users.noreply.github.com>
2023-11-12 20:38:07 -0500

    v3Kevin

--- angular-app-grupal/src/app/servicios/servicios.component.ts
+++ angular-app-grupal/src/app/servicios/servicios.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.css']
 })
-export class ServiciosComponent {
+export class ServiciosComponent implements OnInit {
+
+  services = [
+    {
+      name: 'Ortodoncia',
+      description: 'Tratamiento para corregir la posición de los dientes',
+      image: 'assets/img/servicio1.jpg'
+    },
+    {
+      name: 'Cirugía dental',
+      description: 'Tratamientos quirúrgicos para la boca y los dientes',
+      image: 'assets/img/servicio2.jpg'
+    },
+    {
+      name: 'Endodoncia',
+      description: 'Tratamiento para salvar los dientes con caries profundas',
+      image: 'assets/img/servicio3.jpg'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() { }
 
 }
